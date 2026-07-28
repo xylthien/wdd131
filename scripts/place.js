eefb11d498b1e601e1c4cdbc60374d843f9273c9
xylthien <xylthien@gmail.com>
2026-07-28 09:49:58 -0600

    W03 Project

--- scripts/place.js
+++ scripts/place.js
@@ -4,3 +4,25 @@ document.getElementById("lastModified").innerHTML = document.lastModified;
 const today = new Date();
 
 currentyear.innerHTML = today.getFullYear();
+
+const temperature = 8;
+const windSpeed = 15;
+
+document.getElementById("temperature").textContent = temperature;
+document.getElementById("windSpeed").textContent = windSpeed;
+
+function calculateWindChill(temp, speed) {
+    return (35.74 +
+        (0.6215 * temp) -
+        (35.75 * Math.pow(speed, 0.16)) +
+        (0.4275 * temp * Math.pow(speed, 0.16))
+    ).toFixed(1)
+}
+
+let windChill = "N/A";
+
+if (temperature <= 50 && windSpeed > 3) {
+    windChill = `${calculateWindChill(temperature, windSpeed)}°F`;
+}
+
+document.getElementById("windChill").textContent = windChill;
